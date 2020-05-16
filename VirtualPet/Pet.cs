5d6c47732c81217c8b0e08f4d8511153ace54f78
Chuck R <rapmastercm@aol.com>
2020-05-16 12:17:06 -0400

    Added GetName() and Species

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace VirtualPet
@@ -7,10 +8,17 @@ namespace VirtualPet
     public class Pet
     {
         public string Name { get; set; }
+        public string Species { get; set; }
 
         public void SetName(string name)
         {
             Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+
         }
 
     }
