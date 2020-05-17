96fb920cf9c57604aebbbae9115c8462563bb2b9
Chuck R <rapmastercm@aol.com>
2020-05-17 13:45:18 -0400

    Created code for virtual pet to run(with Josh help)

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -6,7 +6,25 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            Pet newPet = new Pet();
+
             Console.WriteLine("Hello! Welcome to Virtual Pets");
+            Console.WriteLine("1. Feed");
+            Console.WriteLine("2. Play");
+            Console.WriteLine("3. Take to Doctor");
+            string userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                case "1":
+                    {
+                        newPet.Feed();
+                        Console.WriteLine("You've fed your Pet");
+                        break;
+
+                    }
+
+            }
         }
+
     }
 }
