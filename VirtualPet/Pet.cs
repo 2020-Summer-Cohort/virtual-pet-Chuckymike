aa92c98d6e565fc3d22dcf859c0ddb55304d1396
Chuck R <rapmastercm@aol.com>
2020-05-17 13:11:37 -0400

    Completed tests

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -9,7 +9,17 @@ namespace VirtualPet
     {
         public string Name { get; set; }
         public string Species { get; set; }
+        public int Hunger { get; set;}
+        public int Boredom { get; set;}
+        public int Health { get; set; }
 
+        public Pet()
+        {
+            Hunger = 50;
+            Boredom = 60;
+            Health = 30;
+
+        }
         public void SetName(string name)
         {
             Name = name;
@@ -18,6 +28,15 @@ namespace VirtualPet
         {
             Species = Cat;
         }
+        public int GetHunger()
+        {
+            return Hunger;
+        }
+        public string GetSpecies() 
+        {
+            return Species;   
+        }
+
 
         public string GetName()
         {
@@ -25,5 +44,41 @@ namespace VirtualPet
 
         }
 
+        public int GetBoredom()
+        {
+            return Boredom;
+        }
+
+        public int GetHealth()
+        {
+            return Health;
+        }
+
+        public void Feed()
+        {
+            Hunger -= 40;    
+        }
+
+        public void SeeDoctor()
+        {
+
+            Health += 30;
+        }
+
+        public void Play()
+        {
+             Hunger += 10;
+             Boredom -= 20;
+             Health += 10;
+
+            
+        }
+
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+        }
     }
 }
