14dcf4a46509b862b2ce4861d2f27aaeca92fdc6
Chuck R <rapmastercm@aol.com>
2020-05-22 14:28:08 -0400

    added a pet to the shelter

---
+++ VirtualPet.Tests/PetShelterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VirtualPet.Tests
+{
+    public class PetShelterTests
+    {
+        Pet newPet;
+        PetShelter newPetShelter;
+        public PetShelterTests()
+        {
+            newPet = new Pet();
+            newPetShelter = new PetShelter();
+
+        }
+        [Fact]
+        public void Add_Pet_To_Shelter_Should_Add_Pet_To_Shelter()
+        {
+            newPetShelter.AddPetToShelter(newPet);
+
+            Assert.NotNull(newPetShelter.listOfPets[0]);
+        }
+
+    }
+    
+}
---
+++ VirtualPet/PetShelter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPet
+{
+    public class PetShelter
+    {
+        public List<Pet> listOfPets = new List<Pet>();
+
+        public void AddPetToShelter(Pet pet)
+        {
+            listOfPets.Add(pet);
+            Console.WriteLine("You've added a pet to the shelter");
+        }
+    }
+
+}
